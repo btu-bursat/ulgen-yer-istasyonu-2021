4e21d05b783fc35f5ab5da6cd7e4b9e38c946cd2
Kerem Ersu <56276464+keremersu35@users.noreply.github.com>
2021-08-07 10:23:52 +0300

    Add files via upload

---
+++ MplWidget.py
@@ -0,0 +1,89 @@
+from PyQt5.QtWidgets import*
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.figure import Figure
+   
+class MplWidget(QWidget):
+    
+    def __init__(self, parent = None):
+
+        QWidget.__init__(self, parent)
+        
+        self.canvas = FigureCanvas(Figure())
+        
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+        
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
+
+class sicaklikWidget(QWidget):
+    
+    def __init__(self, parent = None):
+
+        QWidget.__init__(self, parent)
+        
+        self.canvas = FigureCanvas(Figure())
+        
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+        
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
+
+class basincWidget(QWidget):
+    
+    def __init__(self, parent = None):
+
+        QWidget.__init__(self, parent)
+        
+        self.canvas = FigureCanvas(Figure())
+        
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+        
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
+
+class pilGerilimiWidget(QWidget):
+    
+    def __init__(self, parent = None):
+
+        QWidget.__init__(self, parent)
+        
+        self.canvas = FigureCanvas(Figure())
+        
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+        
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
+
+class hizWidget(QWidget):
+    
+    def __init__(self, parent = None):
+
+        QWidget.__init__(self, parent)
+        
+        self.canvas = FigureCanvas(Figure())
+        
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+        
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
+
+class gpsAltitudeWidget(QWidget):
+    
+    def __init__(self, parent = None):
+
+        QWidget.__init__(self, parent)
+        
+        self.canvas = FigureCanvas(Figure())
+        
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+        
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
+
+        
